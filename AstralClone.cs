9d9fe0001ee465d3a00415d3279e106f194e311a
xKiraiChan <xKiraiChan@gmail.com>
2021-10-23 01:03:55 -0400

    initial commit

---
+++ AstralClone.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+
+[assembly: MelonInfo(typeof(Astrum.AstralClone), "AstralClone", "0.1.0", downloadLink: "github.com/Astrum-Project/AstralClone")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(ConsoleColor.DarkMagenta)]
+
+namespace Astrum
+{
+    public class AstralClone : MelonMod
+    {
+        public override void OnApplicationStart() =>
+            HarmonyInstance.Patch(
+                typeof(VRC.Core.APIUser).GetProperty(nameof(VRC.Core.APIUser.allowAvatarCopying)).GetSetMethod(),
+                new HarmonyLib.HarmonyMethod(typeof(AstralClone).GetMethod(nameof(AstralClone.Hook), BindingFlags.NonPublic | BindingFlags.Static))
+            );
+
+        private static void Hook(ref bool __0) => __0 = true;
+    }
+}
